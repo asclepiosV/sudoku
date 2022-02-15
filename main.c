09de4a1684261efc3fec8fec371b1ed4bd9f25c7
asclepiosV <poub.prod@gmail.com>
2022-02-15 09:46:30 +0200

    smart inputs

--- main.c
+++ main.c
@@ -4,8 +4,39 @@ void board();
 int main() {
     int a = 0;
     int b = 0;
-    printf("Add number: \n");
-    scanf("%d%d", &a, &b);
+    char choice;
+    printf("Choose a or b\n");
+    scanf("%s", &choice);
+    switch (choice) {
+        case 'a':
+            printf("Add a number: \n");
+            scanf("%d", &a);
+            switch (a) {
+                case 1:
+                    b = 2;
+                    break;
+                case 2:
+                    b = 1;
+                    break;
+            }
+            break;
+        case 'b':
+            printf("Add a number: \n");
+            scanf("%d", &b);
+            switch (b) {
+                case 1:
+                    a = 2;
+                    break;
+                case 2:
+                    a = 1;
+                    break;
+            }
+            break;
+        default:
+            printf("STP choisi entre a et b la prochaine fois");
+    }
+
+
     board(a,b);
     return 0;
 }
